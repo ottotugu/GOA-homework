28853fcfb1ac8b0f78f324965dfeb78f921c7c69
ottotugu <oto.tugushi2009009@gmail.com>
2025-04-24 23:30:36 +0400

    third commit

---
+++ day 2/claswork/classwork.py
@@ -0,0 +1,17 @@
+
+box1 = "goa"
+print(box1)
+
+box2 = 1000000
+print(box2)
+
+box3 = 25.0
+print()
+
+print(100 + 225)
+print(385 -235)
+print(18 * 4)
+print(300 // 7)
+print(300 / 7)
+print(625 // 25)
+print(625 / 25)
---
+++ day 2/homework/homework.py
@@ -0,0 +1,55 @@
+
+str0 = "Otari" 
+str1 = "Tugushi"
+str2 = "Goal Oriented Academy"
+
+print(str0)
+print(str1)
+print(str2)
+
+
+int0 = 14
+int1 = 2345987
+int2 = 8008
+
+print(int0)
+print(int1)
+print(int2)
+
+
+float0 = 0.20909
+float1 = 9.2034
+float2 = 10.203840
+
+print(float0)
+print(float1)
+print(float2)
+
+
+problem0 = 1209 + 3945
+problem1 = 43223 - 320
+problem2 = 86 * 45
+problem3 = 144 / 12 
+problem4 = 234089 + 9234
+problem5 = 203498 - 9343
+problem6 = 9009 * 32
+problem7 = 8008 / 8 
+
+print(problem0)
+print(problem1)
+print(problem2)
+print(problem3)
+print(problem4)
+print(problem5)
+print(problem6)
+print(problem7)
+
+
+
+word0 = "Gamarjoba"
+word1 = " chemi"
+word2 = " saxelia"
+word3 = " Otari"
+
+print(word0 + word1 + word2 + word3)
+
