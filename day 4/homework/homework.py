71bf31a3c9e9fb3c5726312b705ba9900667f737
ottotugu <oto.tugushi2009009@gmail.com>
2025-05-07 17:06:54 +0400

    sixth commit

--- day 4/homework/homework.py
+++ day 4/homework/homework.py
@@ -11,3 +11,23 @@
 #True თუ წერის სადმე გამოვა True
 
 print(True and 6<7 or "hello" != 5.4 and False or 7-7 < 9+2)
+#  True and True არის True
+#  "hello != 5.4" არის True 
+#  ამიტომაც True or True იქნება True
+#  True and False იქნება False
+#  True or False იქნება True
+
+# integer-ით და integer-ით გამოიყენება ნებისმიერი შედარების ოპერატორები
+# integer-ით და float-ით გამოიყნება ნებისმიერი შედარების ოპერატორები
+# integer-ით და string-ით გამოიყენება მხოლოდ != 
+# string-ით და string-ით გამოიყენება == და !=
+
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
+
+print(True or False)
+print(True or True)
+print(False or True)
+print(False or False)
---
+++ day 5/notes/notes.py
@@ -0,0 +1,11 @@
+
+#web development --> საიტების შექმნა
+#frontend
+#backend
+
+#html --> სტრუქტურა
+#css --> დიზაინი
+#js --> ფუნქციონალი
+
+#index.html
+
