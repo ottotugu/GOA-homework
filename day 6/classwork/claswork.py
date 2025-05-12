fe50f4dbe962ef1579a737e0d1437ca06f5d1bd8
ottotugu <oto.tugushi2009009@gmail.com>
2025-05-12 15:27:14 +0400

    seventh commit

---
+++ day 6/classwork/claswork.py
@@ -0,0 +1,10 @@
+
+x = 10
+print(x > 5 and x < 15) #გამოიტანს True რადგან ორივე არის True და შუაში მოცემულია and
+
+age = 17
+print(age < 18 or age > 65) #გამოიტანს True რადგან შუაშია or და მათგან ერთ-ერთი არის True
+
+password = "admin"
+print(password != "1234" and password != "password")#გამოიტანს True რადგან ორივე არის 
+                                                      #True და შუაში მოცემულია and
