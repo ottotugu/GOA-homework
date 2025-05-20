7b2dfee663ac9ea453ebba216038f1b3db3b7fac
ottotugu <oto.tugushi2009009@gmail.com>
2025-05-20 23:38:05 +0400

    tenth commit

---
+++ day 9/classwork/classwork.py
@@ -0,0 +1,21 @@
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i + 1
+
+i = 10
+
+while i >= 1:
+    print(i)
+    i = i - 1
+
+i = 0
+while i < 1:
+    print('cavatanem')
+    i = i - 1
+
+
+
+#while loop ამეორებს ერთი და იმავეს სანამ რაიმე ბრძანება არ გააჩერებს
+
---
+++ day 9/classwork/notes.py
@@ -0,0 +1,19 @@
+i = 0
+
+while i > 10:
+    print("Otari")
+    i = i - 1
+
+
+password = "1234"
+use_password = input('please enter your password: ')
+while use_password != password:
+    use_password = input('please try again: ') 
+    
+print('Acces Granted')  
+
+while True:
+    print("cavatanem")
+
+
+
