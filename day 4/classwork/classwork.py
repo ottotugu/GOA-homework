a97917840ddfdf73cccba0f4f8141f0a45ec1e7f
ottotugu <oto.tugushi2009009@gmail.com>
2025-05-31 18:34:17 +0400

    4

--- day 4/classwork/classwork.py
+++
@@ -1,9 +0,0 @@
-
-age = int(input("Enter age: "))
-
-print(age >= 18)
-
-
-
-print(True and False or 5>2 and False or False and True) #გამოიტანს false რადგან ბოლოში მიწერილი 
-                                                    #აქ false or false ამიტომაც მკოდმა ამაზე მიიღო გადაწყვეტილება
