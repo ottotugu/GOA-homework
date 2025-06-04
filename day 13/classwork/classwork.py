29bb286ff6fb94b706fc02220f6966744bd077c4
ottotugu <oto.tugushi2009009@gmail.com>
2025-06-04 18:54:43 +0400

    morer

---
+++ day 13/classwork/classwork.py
@@ -0,0 +1,38 @@
+
+number = int(input("enter 1 number: "))
+number1 = int(input("enter 1 more number: "))
+number2 = int(input("enter 1 more: "))
+added = number + number2 + number2
+print(added)
+
+i = 10
+
+while i >= 1:
+    print(i)
+    i = i - 1
+
+ik = 0 
+for i in range(1, 101,):
+    ik += i
+    print(ik)
+
+
+dd = int(input("enter number: "))
+while dd >= 1:
+    print(dd)
+    dd = dd - 1
+
+kk = int(input("enter number: "))
+for i in range(kk):
+     print(kk)
+     kk = kk - 1
+
+    
+for i in range(2, 50 ,2):
+     print(i)
+
+for i in range(2, 100 ,2):
+     print(i)
+
+for i in range(10):
+     print("გოა ტოპ")
---
+++ day 13/classwork/notes.py
@@ -0,0 +1,12 @@
+
+for i in range(15):
+    print(i)
+
+#while --> სანამ
+
+counter = nigger 
+
+while counter < 10 :
+    print(counter)
+    counter += 1 #counter = counter + 1
+    
