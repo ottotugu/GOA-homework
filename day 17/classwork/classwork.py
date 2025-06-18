a60fbf5395f6644027c4f4e979ed8b63c0d47bf6
ottotugu <oto.tugushi2009009@gmail.com>
2025-06-18 23:02:48 +0400

    goa

---
+++ day 17/classwork/classwork.py
@@ -0,0 +1,14 @@
+
+kishka = ["khinkali", "burger", "mwvadi", "pizza"]
+
+color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+animal = ["pig", "cow", "sheep", "dog", "cat", " hamster"]
+print(animal[2])
+print(animal[1])
+
+number1 = [1, 2, 3, 4, 5]
+number2 = [1, 2, 3, 4, 5]
+print(number1[2] + number2[3])
+
+# სია არის მონაცემები შენახული ცვლადში სადაც ყველა მონაცემს თავისი index გააჩნია
---
+++ day 17/homework/homework.py
@@ -0,0 +1,13 @@
+
+fruit = ["banana", "watermelon", "apple", "peach"]
+print(fruit)
+# ცვლადი ერთ მონაცებს შეიცავს, list კი სიას სადაც რამდენიმე მონაცემია
+
+ioanes_tolmebi = ["ისხვის ტოლმა(ფავ)", 
+                  "ვაზის ფოთლის ტოლმა", 
+                  "კომბოსტოს ტოლმა", 
+                  "ღორის ხორცის ტოლმა",
+                  "საქონლის ხორცის ტოლმა"
+                  ]
+print(ioanes_tolmebi[4])
+print(ioanes_tolmebi[2])
