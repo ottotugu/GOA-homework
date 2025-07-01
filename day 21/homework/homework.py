e0a7c9b38fe68038737d22cde90df3567b4898ac
ottotugu <oto.tugushi2009009@gmail.com>
2025-07-02 00:19:02 +0400

    goa

---
+++ day 21/homework/homework.py
@@ -0,0 +1,23 @@
+i = int(input("ent num:"))
+while i >= 1:
+    print(i+1)
+    i = i - 1
+
+number = int(input("put any number:"))
+
+if number % 2 == 0:
+    print("luwia")
+else:
+    print("kentia")
+
+num1 = int(input("put any number:"))
+num2 = int(input("put any number:"))
+if num1 > num2:
+    print(num1)
+elif num2 > num1:
+    print(num2)
+
+name = int(input("yo name:"))
+for i in range(10):
+    print(name)
+
