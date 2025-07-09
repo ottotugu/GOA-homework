570622e95b48a00f2acba07bf7fc4a72adaa5699
ottotugu <oto.tugushi2009009@gmail.com>
2025-07-10 03:15:51 +0400

    goa

---
+++ day 23/classwork/notes.py
@@ -0,0 +1,21 @@
+name = input("yo name: ")
+def greet(name):
+    print("zd " + name)
+
+greet(name)
+
+
+def nigger():
+   num = 2 + 2
+   return num 
+print(nigger())
+
+# return აბრუნებს მნიშვნელობას
+
+
+
+def f1():
+    return 10
+def f2(num):
+    return num * 2
+print(f2)
---
+++ day 23/homework/homework.py
@@ -0,0 +1,35 @@
+def greet():
+    print("გამარჯობა")
+
+greet()
+
+def plus():
+   num = 2 + 2
+   return num 
+print(plus())
+
+num = int(input("ent num: "))
+def whichs(num):
+    if num % 2 == 0:
+        print("luwia")
+    else:
+        print("kentia")
+    
+whichs(num)
+
+name = (input("yo name: "))
+age = int(input("yo age: "))
+def unda():
+    print(name + " aris", age , "wlis")
+
+unda()
+
+
+
+def biggest(a, b, c):
+    return max(a, b, c)
+
+print(biggest(15, 53, 62))
+
+
+
